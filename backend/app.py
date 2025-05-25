c9edfa9a847df51649940107fd606928419288cc
JenifferAlba <119987897+JenifferAlba@users.noreply.github.com>
2025-05-25 16:49:04 -0300

    Corrige app.py e adiciona requirements.txt para deploy

--- backend/app.py
+++ backend/app.py
@@ -14,3 +14,11 @@ if __name__ == '__main__':
     with app.app_context():
         db.create_all()
     app.run(debug=True)
+ import os
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('postgresql://postgres:ZAAjwdzqFyuNVganVwaotgGivnJngaHy@centerbeam.proxy.rlwy.net:55389/railway')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+   
